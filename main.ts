e1d6c7553197d31682ef24db1322585c42e52520
PUTINMYOUREMAIL <88059833+PUTINMYOUREMAIL@users.noreply.github.com>
2021-07-27 15:33:35 -0700

    stuff done leave me aloneees

--- main.ts
+++ main.ts
@@ -1,20 +1,27 @@
 namespace SpriteKind {
     export const powerup = SpriteKind.create()
 }
+statusbars.onZero(StatusBarKind.Health, function (status) {
+    game.over(false, effects.melt)
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
-    info.changeLifeBy(-1)
+    statusbar.value += -5
     scene.cameraShake(4, 500)
     otherSprite.destroy(effects.bubbles)
+    info.changeScoreBy(-3)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    info.changeScoreBy(1)
+    info.changeScoreBy(5)
     sprite.startEffect(effects.hearts, 100)
     otherSprite.destroy()
     music.magicWand.play()
+    statusbar.value += 1
 })
 let projectile: Sprite = null
 let choice = 0
+let statusbar: StatusBarSprite = null
 let mySprite: Sprite = null
+game.splash("Eat The Ditto", "Press A")
 scene.setBackgroundColor(9)
 controller.moveSprite(mySprite, 100, 100)
 mySprite = sprites.create(img`
@@ -37,7 +44,16 @@ mySprite = sprites.create(img`
     `, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 100)
 mySprite.setStayInScreen(true)
-info.setLife(3)
+statusbar = statusbars.create(20, 4, StatusBarKind.Health)
+statusbar.attachToSprite(mySprite)
+statusbar.max = 50
+forever(function () {
+    if (info.score() >= 100) {
+        game.over(true)
+    } else {
+    	
+    }
+})
 game.onUpdateInterval(500, function () {
     choice = randint(1, 3)
     if (choice == 1) {
